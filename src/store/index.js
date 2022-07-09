fb644410c073cdc13d71c917d573e4181c765fbb
SPY_STAR <91047452+SPY-STAR0003@users.noreply.github.com>
2022-07-09 18:44:06 +0430

    fix a bug in redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import users from "./slices/usersSlice";
 import articles from "./slices/articlesSlice";
-import auth from "./slices/authSlice";
 
 export const store = configureStore({
     reducer : {
         articles,
         users,
-        auth
     }
 })
